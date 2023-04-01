5bf0bbc36cc0a9d4891d533311c3edfb67f4219b
Pearson <spearson@lwsd.org>
2023-04-01 10:35:23 -0700

    newchanges

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -25,5 +25,7 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    
     window.deferredPrompt = null;
+
 });
