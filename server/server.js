9e2e2ec19b998af1081d6c7593ef49a9b09c4497
Pearson <spearson@lwsd.org>
2023-04-01 14:40:05 -0700

    new

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static('./dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
